6b77a259b7d71672c0f15ed2b9874952df94011b
のびすけ <rsksound@gmail.com>
2016-12-21 17:19:19 +0900

    init

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const HTTP = require('http');
+const PORT = process.env.port || 3000;
+
+HTTP.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js!\n');
+}).listen(PORT);
