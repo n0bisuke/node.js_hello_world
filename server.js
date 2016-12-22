a742a8f278e7d3b548b293f725c5cdb70b57b397
のびすけ <rsksound@gmail.com>
2016-12-23 00:22:53 +0900

    update

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const HTTP = require('http');
-const PORT = process.env.PORT || 3000;
+console.log('aaa');
 
-HTTP.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js!\n');
-}).listen(PORT);
+// const HTTP = require('http');
+// const PORT = process.env.PORT || 3000;
+
+// HTTP.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello Node.js!\n');
+// }).listen(PORT);
