d30f499a46257fde16f3ebae62366c0357c400e6
のびすけ <rsksound@gmail.com>
2016-12-23 10:24:39 +0900

    update

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 HTTP.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js!\n');
+  res.end('Hello Node.js!!\n');
 }).listen(PORT);
